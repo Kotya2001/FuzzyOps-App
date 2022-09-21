170ee3135c567f15b3973cee5247dac4d00cf352
ilya Belozerov <iabelozerov@quasarpro.ru>
2022-09-21 13:04:35 +0300

    Additions

---
+++ .history/frontend/my-app/src/pages/Login_20220918174900.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+// import '../src/Styles.css';
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div className="inputForm">
+                <input type="text" placeholder="Email" className="type-1"/>
+                <input type="text" placeholder="Пароль"/>
+                <p>
+                    <Link to='/register'>Register</Link>
+                </p>
+            </div>
+        )
+    }
+}
